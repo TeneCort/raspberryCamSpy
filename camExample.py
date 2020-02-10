4e9fde0e8e37ce68bab6821741d4798d188b6d48
tenecort <karapostolis.ioannis@gmail.com>
2020-02-10 12:02:32 +0000

    changed to fullscreen

--- camExample.py
+++ camExample.py
@@ -5,6 +5,9 @@ import curses
 import time
 import RPi.GPIO as GPIO
 
+# Initialising a full screen window
+cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
@@ -76,7 +79,7 @@ while(cap.isOpened()):
                 frame[y, random.randint(fWidth-1, fWidth+1)] = [c, c ,c]
         
         # Display the resulting frame
-        cv2.imshow('Frame', apply_grayScale(frame))
+        cv2.imshow('window', apply_grayScale(frame))
         
         fWidth += 1
         
